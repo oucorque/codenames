7478cc0a11734e95b9649e47eacf415ce3e711f5
Olivier Ribardière <4162940+ribardo0@users.noreply.github.com>
2018-08-17 14:59:35 +0200

    Corrections erreurs de compilation dans Game.ts

--- src/app/beans/Game.ts
+++ src/app/beans/Game.ts
@@ -2,46 +2,49 @@ import { Action } from './action';
 
 export class Game {
 
-    sizex: number = 5;
-    sizey: number = 5;
+    public sizex = 5;
+    public sizey = 5;
 
-    words: string[sizex][sizey]; // Le tableau des mots communs
-    card1: number[sizex][sizey]; // La liste des couleurs des mots du joueur 1 (0 = case jaune, 1 = case verte, 2 = case noire)
-    card2: number[sizex][sizey]; // Idem joueur 2
-    actions: List<Action>;
+    public words: string[][]; // Le tableau des mots communs
+    public card1: number[][]; // La liste des couleurs des mots du joueur 1 (0 = case jaune, 1 = case verte, 2 = case noire)
+    public card2: number[][]; // Idem joueur 2
+    public actions: Array<Action>[];
 
- 	public initNewGame(): Game {
-		allWords: string[30] = ....;
-		// TODO Thomas : remplir words et card1 et 2
-	}
-	
-    public calculateNewGame(action: Action): Game {
-        if (action.isWord || action.endPlayer) {
-            this.curPlayer = Game.reversePlayer(this.curPlayer);
-        }
-        return this;
-    }
-
-	// La liste des statuts des mots (0 si pas trouvés, 1 si vert, 2 si neutre)
-	public getWordStatus(): number[sizex][sizey] {
-		
-	
-	}
-	
-	public getCurPlayer(): number {
-		
-	}
-	
-	public isEnded(): boolean {
-		
-	}
-	
     private static reversePlayer(player: number): 2 | 1 {
         if (player === 1) {
             return 2;
         } else {
             return 1;
         }
+    }
+
+    public initNewGame(): Game {
+        // allWords: string[30] = ....;
+        // TODO Thomas : remplir words et card1 et 2
+        return new Game();
+    }
+
+    /**
+     * Cette méthode renvoie le nouveau jeu après avoir appliqué l'action choisie
+     * @param action l'action à appliquer sur le jeu en cours
+     */
+    public calculateNewGame(action: Action): Game {
+
+        return this;
+    }
+
+    // La liste des statuts des mots (0 si pas trouvés, 1 si vert, 2 si neutre)
+    public getWordStatus(): number[][] {
+        return null; // FIXME A coder
+
+    }
+
+    public getCurPlayer(): number {
+        return 1; // FIXME A coder
+    }
+
+    public isEnded(): boolean {
+        return true; // FIXME A Coder
     }
 
 }
